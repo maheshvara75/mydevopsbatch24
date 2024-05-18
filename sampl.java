2ee3faa46942bff35fb597b64bff342b73e2fc00
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-05-18 11:11:48 +0530

    Update sampl.java

--- sampl.java
+++ sampl.java
@@ -1,1 +1,13 @@
-This is JVAVA file.
+//my jenkins sample java file
+class HelloWorld
+{
+    public static void main(String []args)
+    {
+        System.out.println("Java sample code that runs from Jenkins.");
+        System.out.println("Added second line to check Poll SCM option - one last time!!.");
+    }
+    
+};
+
+//execute java file today with the build step.
+//this is the demo before adding build step.
