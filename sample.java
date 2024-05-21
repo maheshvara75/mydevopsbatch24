76a26518528a937f4eabf20ac00a1a57b65633c5
maheshvara75 <73717752+maheshvara75@users.noreply.github.com>
2024-05-21 08:22:38 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -4,7 +4,7 @@ class HelloWorld
     public static void main(String []args)
     {
         System.out.println("Java sample code that runs from Jenkins.");
-        System.out.println("Added second line to check Poll SCM option - one last time!!.");
+        System.out.println("Added second line to check Poll SCM option - one last time!!...");
     }
     
 };
